561859b84605d921896cf253806586b020a35787
Bilal <ejramwayne@gmail.com>
2025-08-03 20:33:26 +0530

    Enhance logistic regression documentation: add detailed comments and explanations for model components and loss function

--- src/linear_model/LogisticRegression/logistic_regression.py
+++ src/linear_model/LogisticRegression/logistic_regression.py
@@ -1,3 +1,20 @@
+# Logistic Regression is a classification algorithem that transforms the linear output of a linear model into a probability 
+# it takes input features (which can be numerical or transformed from other data types, like text),
+#  computes a linear combination of these features
+
+# Using the sigmoid function (Formula: 1/1+e^-x) The sigmoid function takes in a real world value and converts it into
+
+
+# A probability that comes between the range of 1 and 0 
+# Threshold: Probability > 0.5 -> Class = 1 else 0 
+
+#---------Example--------#
+
+# In spam detection, logistic regression converts email text into numerical features. 
+# It learns the relationship between these features and whether emails are spam or not,
+#  outputting probabilities so you can classify new emails as "spam" or "not spam".
+
+
 import numpy as np 
 
 def sigmoid(z): 
@@ -9,23 +26,60 @@ def LogisticReg(x, y , epochs = 1000, lr_rate = 1e-2):
   b = 0.0 
 
   for i in range(epochs):
+   
+   
+#------Linear Model---------
    linear_model = w * x + b
+
+
+#------Prediction and Convertion---------
+
    y_pred = sigmoid(linear_model)
 
-   loss = -np.mean(y * np.log(y_pred) + (1 - y) * np.log(1 - y_pred))
+#------Loss Function---------
 
-   w_gradient = np.mean((y_pred - y) * x)
-   b_gradient = np.mean(y_pred - y)
+  """
+
+  Binary (0,1) Cross Entropy Loss 
+  
+  Formula: -1/n[(yi * log(pi)  + (1-yi) log(1-pi)]
+
+ Here: 1/n refers to the mean 
+      yi refers to the true value
+      pi refers to the predicted value 
+
+ How It Works:
+
+ If the true label is 1 and the model predicts a probability close to 1 the loss for that sample is very small
+
+ If the model predicts a probability close to 0 (wrong) when the true label is 1 the loss becomes very large
+
+ The same logic applies for true label 0
+
+ The loss penalizes confident wrong predictions much more than less confident ones
+        
+
+ Why Logarithms: 
+
+ Logarithms strongly punishes the probabilities that are too far from the actual label which encourages the model to produce a 
+ confident and accurate label
+
+  """
+
+  loss = -np.mean(y * np.log(y_pred) + (1 - y) * np.log(1 - y_pred)) 
 
 
-   w -= w_gradient * lr_rate
-   b -= b_gradient * lr_rate
+#------Gradients---------# (Gradients are optimzing algorithem that are mainly used to adjust the value of the weights and bias with respect average of the predicted value, actual value and faetures)
 
-   
+  w_gradient = np.mean((y_pred - y) * x)
+  b_gradient = np.mean(y_pred - y)
 
-   result = sigmoid(y_pred)
 
-   if i % 100 == 0:
+  w -= w_gradient * lr_rate # Adjusting the weight according to the gradient and the learning rate 
+   #                         (The learning rate controls how fast we change the weights/Bias) Too high can make it skip the perfect weight and bais and too low can make it stuck so we have to find the sweet spot
+  b -= b_gradient * lr_rate
+
+  if i % 100 == 0:
      print(f"Epoch {i}: loss = {loss}, predictions = {y_pred}")
 
 
