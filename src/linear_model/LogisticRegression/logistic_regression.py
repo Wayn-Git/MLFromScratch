d8bdcfe7341293433e2a631ab5379e6d0f0441fa
Bilal <ejramwayne@gmail.com>
2025-08-09 19:07:01 +0530

    Fixed the indentation where the main part of the code was outside the loop. Clipped y_pred so it doesn't blow up to infiinity

--- src/linear_model/LogisticRegression/logistic_regression.py
+++ src/linear_model/LogisticRegression/logistic_regression.py
@@ -1,6 +1,6 @@
 # Logistic Regression is a classification algorithem that transforms the linear output of a linear model into a probability 
 # it takes input features (which can be numerical or transformed from other data types, like text),
-#  computes a linear combination of these features
+# computes a linear combination of these features
 
 # Using the sigmoid function (Formula: 1/1+e^-x) The sigmoid function takes in a real world value and converts it into
 
@@ -22,72 +22,55 @@ def sigmoid(z):
 
 
 def LogisticRegression(x, y , epochs = 1000, lr_rate = 1e-2):
-  w = 0  
-  b = 0.0 
+    w = 0   # Doesn't really matter if it's random or 0. Will be optimized later
+    b = 0.0 
 
-  for i in range(epochs):
-   
-   
-#------Linear Model---------
-   linear_model = w * x + b
+    for i in range(epochs):
+       
+        #------Linear Model---------
+        linear_model = w * x + b
 
+        #------Prediction and Convertion---------
+        y_pred = sigmoid(linear_model)
 
-#------Prediction and Convertion---------
+        # Clip to avoid log(0)
+        eps = 1e-15   # Epsilon (eps) in np.clip is just a tiny constant to prevent mathematical errors when taking the log of probabilities.
+        y_pred = np.clip(y_pred, eps, 1 - eps)
 
-   y_pred = sigmoid(linear_model)
-
-#------Loss Function---------
-
-  """
-
-  Binary (0,1) Cross Entropy Loss 
+        #------Loss Function---------
+        """
+        Binary (0,1) Cross Entropy Loss 
   
-  Formula: -1/n[(yi * log(pi)  + (1-yi) log(1-pi)]
+        Formula: -1/n[(yi * log(pi)  + (1-yi) log(1-pi)]
 
- Here: 1/n refers to the mean 
-      yi refers to the true value
-      pi refers to the predicted value 
+        Here: 1/n refers to the mean 
+              yi refers to the true value
+              pi refers to the predicted value 
 
- How It Works:
-
- If the true label is 1 and the model predicts a probability close to 1 the loss for that sample is very small
-
- If the model predicts a probability close to 0 (wrong) when the true label is 1 the loss becomes very large
-
- The same logic applies for true label 0
-
- The loss penalizes confident wrong predictions much more than less confident ones
+        How It Works:
+        If the true label is 1 and the model predicts a probability close to 1 the loss for that sample is very small
+        If the model predicts a probability close to 0 (wrong) when the true label is 1 the loss becomes very large
+        The same logic applies for true label 0
+        The loss penalizes confident wrong predictions much more than less confident ones
         
+        Why Logarithms: 
+        Logarithms strongly punishes the probabilities that are too far from the actual label which encourages the model to produce a 
+        confident and accurate label
+        """
+        loss = -np.mean(y * np.log(y_pred) + (1 - y) * np.log(1 - y_pred)) 
 
- Why Logarithms: 
+        #------Gradients---------# (Gradients are optimzing algorithem that are mainly used to adjust the value of the weights and bias with respect average of the predicted value, actual value and faetures)
+        w_gradient = np.mean((y_pred - y) * x)
+        b_gradient = np.mean(y_pred - y)
 
- Logarithms strongly punishes the probabilities that are too far from the actual label which encourages the model to produce a 
- confident and accurate label
+        w -= w_gradient * lr_rate # Adjusting the weight according to the gradient and the learning rate 
+        #                         (The learning rate controls how fast we change the weights/Bias) Too high can make it skip the perfect weight and bais and too low can make it stuck so we have to find the sweet spot
+        b -= b_gradient * lr_rate
 
-  """
-
-  loss = -np.mean(y * np.log(y_pred) + (1 - y) * np.log(1 - y_pred)) 
-
-
-#------Gradients---------# (Gradients are optimzing algorithem that are mainly used to adjust the value of the weights and bias with respect average of the predicted value, actual value and faetures)
-
-  w_gradient = np.mean((y_pred - y) * x)
-  b_gradient = np.mean(y_pred - y)
-
-
-  w -= w_gradient * lr_rate # Adjusting the weight according to the gradient and the learning rate 
-   #                         (The learning rate controls how fast we change the weights/Bias) Too high can make it skip the perfect weight and bais and too low can make it stuck so we have to find the sweet spot
-  b -= b_gradient * lr_rate
-
-  if i % 100 == 0:
-     print(f"Epoch {i}: loss = {loss}, predictions = {y_pred}")
+        if i % 100 == 0:
+            print(f"Epoch {i}: loss = {loss}, predictions = {y_pred}")
 
 
 x = np.array([1, 2, 3, 4, 5])
 y = np.array([0, 0, 1, 1, 1])
 LogisticRegression(x, y)
-
- 
-  
-  
-  
